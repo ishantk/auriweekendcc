db95aec478d36b9861aef2a86d7b92789e4abf26
Ishant Kumar <er.ishant@gmail.com>
2017-10-29 15:19:44 +0530

    JDBC Intro

---
+++ CJSession/src/com/auribises/db/App.java
@@ -0,0 +1,70 @@
+package com.auribises.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("App Started");
+		
+		Student s1 = new Student();
+		s1.roll = 2;
+		s1.name = "Jennie";
+		s1.age = 21;
+		s1.email = "jennie@example.com";
+		s1.address = "Country Homes";
+		
+		System.out.println(s1.name+" lives in "+s1.address);
+
+		// JDBC
+		
+		try {
+			
+			//1. Load the Driver
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//2. Create the Connection
+			String url = "jdbc:mysql://localhost/auribises";
+			String user = "root";
+			String password = "";
+			
+			Connection con = DriverManager.getConnection(url, user, password);
+			
+			//3. Write the SQL Query
+			//String sql = "insert into Student values("+s1.roll+",'"+s1.name+"',"+s1.age+",'"+s1.email+"','"+s1.address+"')";
+			String sql = "insert into Student values(?,?,?,?,?)";
+			
+			//4. Execute SQL Statement
+			//Statement stmt = con.createStatement();
+			//int i = stmt.executeUpdate(sql);
+			
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, s1.roll);
+			ps.setString(2, s1.name);
+			ps.setInt(3, s1.age);
+			ps.setString(4, s1.email);
+			ps.setString(5, s1.address);
+			
+			int i = ps.executeUpdate();
+			
+			if(i>0){
+				System.out.println(s1.name +" Saved in DB");
+			}else{
+				System.out.println(s1.name +" not saved in DB");
+			}
+			
+			//5. Close the Connection
+			con.close();
+			
+		} catch (Exception e) {
+			System.out.println("Some Error: "+e);
+		}
+		
+		
+		System.out.println("App Finished");
+	}
+
+}
---
+++ CJSession/src/com/auribises/db/Student.java
@@ -0,0 +1,48 @@
+package com.auribises.db;
+
+public class Student {
+
+	// Attributes
+	int roll;
+	String name;
+	int age;
+	String email;
+	String address;
+	
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [roll=" + roll + ", name=" + name + ", age=" + age + ", email=" + email + ", address=" + address
+				+ "]";
+	}
+	
+}
